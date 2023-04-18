422060c8aa1fb884eb744163440411dbb4f30716
sudipto <fedev.sudipto@gmail.com>
2023-04-18 23:50:30 +0530

    Java aptitude questions

--- src/com/java/assesment/JavaAssessment.java
+++ src/com/java/assesment/JavaAssessment.java
@@ -30,7 +30,7 @@ public class JavaAssessment {
 				boolean b1 = true, b2 = false; int i1 = 1, i2 = 2;
 				
 				 (i1 | i2) == 3
-				 i2 && b1
+				 i2 && b1 - correct 
 				 b1 || !b2
 				 (i1 ^ i2) < 4
 		 * */
@@ -47,10 +47,49 @@ public class JavaAssessment {
 		 * When we perform a bitwise OR operation on these two binary values, 
 		 * we get 0011, which is the binary representation of 3. 
 		 * So, the expression (i1 | i2) evaluates to 3
-		 * 0001
-		 * 0010
-		 * -----
-		 * 0011 
+		 *
+		 * */
+		System.out.println(b1 || !b2);// true
+		System.out.println((i1 ^ i2) < 4);// true
+		
+		
+		/**
+		 * Q4. What is the output of this code?
+		1: class Main {
+		2:   public static void main (String[] args) {
+		3:     int array[] = {1, 2, 3, 4};
+		4:     for (int i = 0; i < array.size(); i++) {
+		5:        System.out.print(array[i]);
+		6:     }
+		7:   }
+		8: }
+		
+		 It will not compile because of line 4. - correct
+		 It will not compile because of line 3.
+		 123
+		 1234
+		* */
+		
+		
+		/**
+		 * Q5. Which of the following can replace the CODE SNIPPET to make the code below print "Hello World"?
+		 * 
+			interface Interface1 {
+			    static void print() {
+			        System.out.print("Hello");
+			    }
+			}
+			
+			interface Interface2 {
+			    static void print() {
+			        System.out.print("World!");
+			    }
+			}
+			
+			 super1.print(); super2.print();
+			 this.print();
+			 super.print();
+			 Interface1.print(); Interface2.print();
 		 * */
 		
 	}
